562725ed789d04eb442c2e03a6567047e158e14f
adhamcahyo <nebelwerfersf@gmail.com>
2023-06-03 23:07:47 +0700

    n+1 problem fix

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -11,6 +11,7 @@ class TodoController extends Controller
 {
     public function index()
     {
+        
         $todos = Todo::with('category')->where('user_id', auth()->user()->id)
             ->orderBy('is_complete', 'asc')
             ->orderBy('created_at', 'desc')
