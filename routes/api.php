8dbc3812bd24358f9ec9a1fbc03315928c8c176e
adhamcahyo <nebelwerfersf@gmail.com>
2023-06-16 15:38:56 +0700

    api : auth, read, search data

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::post('/login', [AuthController::class, 'login'])->name('login');
+Route::middleware('auth:sanctum')->group(function() {
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+    Route::get('/profile', [ProfileController::class,'index'])->name('profile');
+    // Route::resource('/todo', TodoController::class);
+    Route::apiResource('/todo', TodoController::class);
 });
